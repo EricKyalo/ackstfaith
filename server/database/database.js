42d475c2c1885b8170ca168b600ebe586446a83b
EricKyalo <89025099+EricKyalo@users.noreply.github.com>
2022-04-05 15:06:05 +0300

    quick fix

--- server/database/database.js
+++ server/database/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 const connectDB =  async() => {
     try{
     const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true, 
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 });
 console.log(`MongoDB Connected: ${db.connection.host}`)
     }catch(err) {
